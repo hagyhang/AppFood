47fa6efcba2720b9fa5e81aadfe76fc48e5f0b51
hagyhang <hagyhang@gmail.com>
2018-05-01 15:01:08 +0700

    addfood

--- controller.js
+++ controller.js
@@ -103,74 +103,116 @@ app.controller("foodCtrl", ($scope, $location, $http)=>{
 			}
 		})
 })
+
 app.controller("addFoods", ($scope, $location, $http) => {
-	$scope.uploadFile = function(e){
-		$scope.$apply(function(scope) {
-			console.log($scope.stepList);
-			var i = e.attributes['tag'].value;
-			$scope.stepList[i].arrImage.push({image: ""});
-			e.files[0] = "";
-        	console.log($scope.stepList);
-	    });
-    };
-	$scope.add = function() {
-	    var f = document.getElementById('file').files[0];
-	    console.log(f);
-        r = new FileReader();
-
-        var fd = new FormData();
-			fd.append("file", f);
-
-			$http({
+	$scope.uploadSubImage = function(e){
+		var i = e.attributes['tag'].value;
+		var f = document.getElementById('file'+i).files[0];
+		var fd = new FormData();
+		fd.append("file", f);
+		console.log("file" + f);
+		$http({
 	        url: "https://cookbook-server.herokuapp.com/image/",
 	        method: 'POST',
 	        data: fd,
 	        headers: { 'Content-Type': undefined, Token: window.localStorage.getItem("FOOD_TOKEN")},
-	        // transformRequest: angular.identity
 	    }).then((res)=>{
 			var data = res.data;
 			if (data.success == true)
 			{
-				console.log(data)
+				let path = "https://cookbook-server.herokuapp.com/" + data.results.path;
+				var img = angular.element(document.querySelector('#img'+i));
+				img.attr('src', path);
+				$scope.food.content[i].arrImage.push({image: path});
+	        	console.log(angular.element(e));
+	        	angular.element(e).val(null);
 			}
 		});
+    };
+	$scope.uploadImage = function() {
+	    var f = document.getElementById('file').files[0];
+	    // console.log(f);
+        var fd = new FormData();
+			fd.append("file", f);
 
-	  //   r.onloadend = function(e) {
-		 //    var data = e.target.result;
-		 //    // document.getElementById("img").src = "data:image/jpg;base64," + data;
-		 //    var fd = new FormData();
-   // 			fd.append("file", data);
-
-   // 			$http({
-		 //        url: "https://cookbook-server.herokuapp.com/image/",
-		 //        method: 'POST',
-		 //        data: fd,
-		 //        headers: { 'Content-Type': undefined, Token: window.localStorage.getItem("FOOD_TOKEN")},
-		 //        // transformRequest: angular.identity
-		 //    }).then((res)=>{
-			// 	var data = res.data;
-			// 	if (data.success == true)
-			// 	{
-			// 		console.log(data)
-			// 	}
-			// });
-	  //   }
-	  //   r.readAsBinaryString(f);
+		$http({
+	        url: "https://cookbook-server.herokuapp.com/image/",
+	        method: 'POST',
+	        data: fd,
+	        headers: { 'Content-Type': undefined, Token: window.localStorage.getItem("FOOD_TOKEN")},
+	    }).then((res)=>{
+			var data = res.data;
+			if (data.success == true)
+			{
+				let path = "https://cookbook-server.herokuapp.com/" + data.results.path;
+				$scope.food.image = path;
+				var i = angular.element(document.querySelector('#img'));
+				i.attr('src', path);
+				i.attr('class', 'food-image center-block');
+				console.log(path);
+			}
+		});
 	}
-
-	var emptyStep = {
-		"step": "",
-		"arrImage": []
+	$scope.add = function(){
+		var materials = $scope.materials.split(";")
+		if ($scope.materials != "" && $scope.food.name != "" && $scope.food.decriptions != "" && $scope.food.content.length > 0)
+		{
+			materials.forEach((m)=>{
+				var ss = m.split(":");
+				var temp = {
+					material: ss[0],
+					amount: ss[1]
+				}
+				$scope.food.materials.push(temp);
+				console.log($scope.food.name);
+				// console.log($scope.food.materials); //amazing !!!!!
+			})
+			var selector = document.getElementById("selector");
+			$scope.food.categoryId = selector.options[ selector.selectedIndex ].value;
+			console.log(selector.options[selector.selectedIndex ].value);
+			console.log($scope.food);
+			$http.post("https://cookbook-server.herokuapp.com/food/" + $scope.food.categoryId + "/create", $scope.food, ConfigToken()).then((res)=>{
+				var data = res.data;
+				if (data.success == true){
+					console.log(data.results);
+				}
+			})
+		}
+		else alert ("Bạn phải nhập đủ thông tin có dấu *");
 	}
-	$scope.stepList = [];
-	$scope.stepList.push(emptyStep);
+	$scope.delStep = function(i){
+		console.log($scope.food.content);
+		$scope.food.content.splice(i, 1);
+	}
+	$scope.addStep = function(i){
+		var emptyStep = {
+			"step": "",
+			"arrImage": []
+		}
+		$scope.food.content.push(emptyStep);
+		console.log($scope.food.content);
+	}
+	$http.get("https://cookbook-server.herokuapp.com/category", ConfigToken()).then((res)=>{
+		var data = res.data;
+		if (data.success == true){
+			$scope.categories = data.results;
+		}
+	})
+	$scope.food={};
+	$scope.food.favourite=[];//
+	$scope.food.name="";
+	$scope.food.categoryId=[];//
+	$scope.food.materials=[];
+	$scope.materials="";
+	$scope.food.content = [];
 })
-app.directive('customOnChange', function() {
-  return {
-    restrict: 'A',
-    link: function (scope, element, attrs) {
-      var onChangeFunc = scope.$eval(attrs.customOnChange);
-      element.bind('change', onChangeFunc);
-    }
-  };
-});
+
+// app.directive('customOnChange', function() {
+//   return {
+//     restrict: 'A',
+//     link: function (scope, element, attrs) {
+//       var onChangeFunc = scope.$eval(attrs.customOnChange);
+//       element.bind('change', onChangeFunc);
+//     }
+//   };
+// });
